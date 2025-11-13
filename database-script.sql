9178e7b4753b6f4cbd6f1e4808d7557d5ee6eef8
joaopaulo120 <joaopaulo1448@gmail.com>
2025-11-13 12:23:43 +0000

    ships

--- database-script.sql
+++ database-script.sql
@@ -1,5 +1,12 @@
 -- TABLES
 
+DROP DATABASE IF EXISTS srd_transportadora_final;
+CREATE DATABASE srd_transportadora_final CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE srd_transportadora_final;
+SELECT DATABASE();
+
+SET FOREIGN_KEY_CHECKS = 0;
+
 CREATE TABLE person(
 	person_id BIGINT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(160) NOT NULL,
@@ -9,8 +16,8 @@ CREATE TABLE person(
     CREATED_AT DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE address(
-	address_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE adress(
+	adress_id BIGINT PRIMARY KEY AUTO_INCREMENT,
     person_id BIGINT,
 	line1 VARCHAR(160) NOT NULL,
 	city VARCHAR(100) NOT NULL,
@@ -50,3 +57,35 @@ INSERT INTO product_types (name, insurance_rate) VALUES
 ('Refrigerated',  0.0020),
 ('Hazmat',        0.0050),
 ('Express',       0.0000);
+
+
+CREATE TABLE shipments(
+  shipment_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  shipper_id BIGINT NOT NULL,
+  origin_address_id BIGINT NOT NULL,
+  dest_address_id BIGINT NOT NULL,
+  goods_desc VARCHAR(240) NOT NULL,
+  weight_kg DECIMAL(12,3) NOT NULL CHECK (weight_kg > 0),
+  volume_m3 DECIMAL(12,3) NOT NULL DEFAULT 0,
+   created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  delivered_at DATETIME NULL,
+FOREIGN KEY (shipper_id) REFERENCES customers(customer_id),
+  FOREIGN KEY (origin_address_id) REFERENCES addresses(address_id),
+  FOREIGN KEY (dest_address_id) REFERENCES addresses(address_id),
+  INDEX idx_ship_dates (created_at, delivered_at)
+) ENGINE=InnoDB;
+
+CREATE TABLE trips (
+  trip_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  vehicle_id BIGINT NOT NULL,
+  driver_id BIGINT NOT NULL,
+  start_at DATETIME NOT NULL,
+  end_at DATETIME,
+  final_weight_kg DECIMAL(12,3) NOT NULL CHECK (final_weight_kg > 0),
+  status ENUM('OPEN','IN_TRANSIT','COMPLETED','CANCELLED') NOT NULL DEFAULT 'OPEN',
+  FOREIGN KEY (vehicle_id) REFERENCES vehicles(vehicle_id),
+  FOREIGN KEY (driver_id)  REFERENCES drivers(person_id)
+) ENGINE=InnoDB;
+
+
+
